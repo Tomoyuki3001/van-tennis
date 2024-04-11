f975fafb9d20d6cc380fc7dce94384246e1c4a00
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-04-10 23:06:43 -0700

    Updated the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-top">
+      <div className="h-screen bg-blue-400">Top</div>
+      <div className="px-20">
+        <div className="h-screen bg-green-400">Middle</div>
+        <div className="h-screen bg-red-400">Bottom</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
