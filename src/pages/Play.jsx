19041b056c4191e1cea153011c79de3c110ca1f9
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-04-16 13:08:28 -0700

    Updated the play page

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -3,6 +3,12 @@ import ContactPage from "../components/ContactPage";
 import Queen from "../assets/play/queen.jpg";
 import Stanley from "../assets/play/stanley.jpg";
 import Slocan from "../assets/play/slocan.jpg";
+import North from "../assets/play/north.jpg";
+import UBC from "../assets/play/ubc.jpg";
+import Facebook from "../assets/play/fb.png";
+import Meetup from "../assets/play/meetup.png";
+import Youtube from "../assets/play/youtube.png";
+import Coach from "../assets/play/coach.jpg";
 
 const Play = () => {
   return (
@@ -11,38 +17,121 @@ const Play = () => {
         <h1 className="lg:text-[8rem] font-bold m-0 text-center">PLAY</h1>
       </div>
       <div className="px-40">
-        <div>
-          <h2>Where can we play?</h2>
+        <div className="flex flex-col items-center my-40 text-center">
+          <h2 className="text-[3rem] font-bold">Where can we play?</h2>
           <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              libero ea at dolores blanditiis consectetur quasi nesciunt
-              doloremque minus temporibus!
+            <p className="px-40 my-10">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta
+              unde optio possimus aperiam quas quidem vel accusamus cumque
+              placeat quisquam debitis ipsum, facilis, inventore, enim quis
+              ratione totam vitae dignissimos!
             </p>
             <div>
-              <div>
-                <h3>Outdoor courts</h3>
-                <div className="flex">
-                  <div>
-                    <img className="w-64 h-auto" src={Stanley} alt="" />
-                    <h4>Stanley Park</h4>
+              <h3 className="my-10">Outdoor courts</h3>
+              <div className="grid lg:grid-rows-1 sm:grid-rows-3 grid-flow-col gap-16">
+                <div>
+                  <div className="flex flex-col items-center">
+                    <img className="w-[30rem] h-[20rem]" src={Stanley} alt="" />
                   </div>
-                  <div>
-                    <img className="w-64 h-auto" src={Queen} alt="" />
-                    <h4>Queen Elizabeth Park</h4>
+                  <h4 className="my-10">Stanley Park</h4>
+                </div>
+                <div>
+                  <div className="flex flex-col items-center">
+                    <img className="w-[30rem] h-[20rem]" src={Queen} alt="" />
                   </div>
-                  <div>
-                    <img className="w-64 h-auto" src={Slocan} alt="" />
-                    <h4>Slocan Park</h4>
+                  <h4 className="my-10">Queen Elizabeth Park</h4>
+                </div>
+                <div>
+                  <div className="flex flex-col items-center">
+                    <img className="w-[30rem] h-[20rem]" src={Slocan} alt="" />
                   </div>
+                  <h4 className="my-10">Slocan Park</h4>
                 </div>
               </div>
-              <h3>Indoor courts</h3>
+              <h3 className="my-10">Indoor courts</h3>
+              <div className="grid grid-cols-2 gap-16 px-80">
+                <div className="">
+                  <div className="flex flex-col items-center">
+                    <img className="w-full h-[20rem]" src={North} alt="" />
+                  </div>
+                  <h4 className="my-10">North Vancouver Tennis Centre</h4>
+                </div>
+                <div className="">
+                  <div className="flex flex-col items-center">
+                    <img className="w-full h-[20rem]" src={UBC} alt="" />
+                  </div>
+                  <h4 className="my-10">UBC Tennis Centre</h4>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div>People</div>
-        <div>Coach</div>
+        <div className="flex flex-col items-center my-40">
+          <h2 className="text-[3rem] font-bold">How to find tennis lovers?</h2>
+          <p className="text-center px-40 my-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+            ipsum quaerat at repellat dolore illo earum expedita suscipit odio
+            dolores reiciendis, iusto similique nesciunt explicabo nobis ad
+            esse, natus velit?
+          </p>
+          <div className="my-20 grid lg:grid-rows-1 sm:grid-rows-2 grid-flow-col px-32">
+            <div className="flex flex-col items-center text-center">
+              <img className="w-48" src={Facebook} alt="" />
+              <h2 className="my-10">Facebook</h2>
+              <p className="w-1/2">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                porro quo architecto nulla quisquam provident, nesciunt sit
+                placeat neque modi.
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <img className="w-48" src={Meetup} alt="" />
+              <h2 className="my-10">Meetup</h2>
+              <p className="w-1/2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quisquam harum quos deleniti voluptas fugit officiis voluptatem
+                necessitatibus adipisci, qui explicabo!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center my-40">
+          <h2 className="text-[3rem] font-bold">How to improve your tennis?</h2>
+          <p className="text-center px-40 my-10">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
+            tempora molestias dolore iusto autem error fuga neque explicabo,
+            amet voluptas quia assumenda adipisci! Atque veritatis ipsa totam
+            mollitia, fugiat dolore!
+          </p>
+          <div>
+            <div className="flex my-20">
+              <div className="w-1/2 flex flex-col items-center">
+                <img className="w-[30rem]" src={Coach} alt="" />
+              </div>
+              <div className="w-1/2">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Natus libero sapiente doloribus itaque sunt quasi porro ipsam
+                  aliquid quidem tenetur minus odio, aspernatur, debitis
+                  repellat perferendis, at vel distinctio quis.
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="w-1/2 flex flex-col items-center">
+                <img className="w-[30rem]" src={Youtube} alt="" />
+              </div>
+              <div className="w-1/2">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Natus libero sapiente doloribus itaque sunt quasi porro ipsam
+                  aliquid quidem tenetur minus odio, aspernatur, debitis
+                  repellat perferendis, at vel distinctio quis.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <ContactPage />
     </div>
