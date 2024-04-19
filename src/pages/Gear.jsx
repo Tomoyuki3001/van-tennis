7d1b550d51c7d133ffdba024d7b5293b962f958b
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-04-19 14:52:49 -0700

    Updated the gear page

--- src/pages/Gear.jsx
+++ src/pages/Gear.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import ContactPage from "../components/ContactPage";
+import Ball from "../assets/gear/ball.png";
+import Racket from "../assets/gear/racket.jpg";
+import Shoes from "../assets/gear/shoes.png";
+import Wear from "../assets/gear/wear.jpg";
+import Towel from "../assets/gear/towel.jpg";
+import Rackets from "../assets/gear/rackets.jpg";
+import String from "../assets/gear/string.jpg";
+import RR from "../assets/gear/rr.jpg";
+import K8 from "../assets/gear/k8.jpg";
+import Tad from "../assets/gear/tad.png";
 
 const Gear = () => {
   return (
@@ -21,7 +31,7 @@ const Gear = () => {
               <div className="grid lg:grid-rows-1 sm:grid-rows-3 grid-flow-col lg:px-20">
                 <div className="mb-10">
                   <div className="flex flex-col items-center">
-                    {/* <img className="w-[30rem] h-[20rem]" src={Ao} alt="" /> */}
+                    <img className="w-[15rem] h-[20rem]" src={Ball} alt="" />
                   </div>
                   <h4 className="mt-5">Balls</h4>
                   <p>
@@ -32,7 +42,7 @@ const Gear = () => {
                 </div>
                 <div className="mb-10">
                   <div className="flex flex-col items-center">
-                    {/* <img className="w-[30rem] h-[20rem]" src={Rg} alt="" /> */}
+                    <img className="w-[20rem] h-[20rem]" src={Racket} alt="" />
                   </div>
                   <h4 className="mt-5">Rackets</h4>
                   <p>
@@ -43,7 +53,7 @@ const Gear = () => {
                 </div>
                 <div className="mb-10">
                   <div className="flex flex-col items-center">
-                    {/* <img className="w-[30rem] h-[20rem]" src={Wb} alt="" /> */}
+                    <img className="w-[30rem] h-[20rem]" src={Shoes} alt="" />
                   </div>
                   <h4 className="mt-5">Shoes</h4>
                   <p>
@@ -54,68 +64,107 @@ const Gear = () => {
                 </div>
               </div>
               <div>
+                <h3>Other equipments</h3>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Tempore veritatis deserunt quibusdam nulla autem repellat et,
                   animi minus quae optio.
                 </p>
-                <div className="flex flex-col items-center">
-                  <div className="flex">
-                    <div className="mb-10">
-                      <div className="flex flex-col items-center">
-                        {/* <img className="w-[30rem] h-[20rem]" src={Rg} alt="" /> */}
-                      </div>
-                      <h4 className="mt-5">French Open</h4>
-                      <h5>late May to early June</h5>
+                <div className="grid lg:grid-rows-1 sm:grid-rows-2 grid-flow-col lg:px-20">
+                  <div className="mb-10">
+                    <div className="flex flex-col items-center">
+                      <img className="w-[20rem] h-[20rem]" src={Wear} alt="" />
                     </div>
-                    <div className="mb-10">
-                      <div className="flex flex-col items-center">
-                        {/* <img className="w-[30rem] h-[20rem]" src={Rg} alt="" /> */}
-                      </div>
-                      <h4 className="mt-5">French Open</h4>
-                      <h5>late May to early June</h5>
+                    <h4 className="mt-5">Clothes</h4>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Hic recusandae blanditiis temporibus obcaecati eum,
+                      perferendis quas error. Nam, exercitationem laboriosam!
+                    </p>
+                  </div>
+                  <div className="mb-10">
+                    <div className="flex flex-col items-center">
+                      <img className="w-[20rem] h-[20rem]" src={Towel} alt="" />
                     </div>
+                    <h4 className="mt-5">Other equipments</h4>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Est aspernatur dolorem id repudiandae. Quidem a dolorum
+                      cumque quasi tempora iusto!
+                    </p>
                   </div>
                 </div>
               </div>
-              <h3 className="my-10">ATP/WTA Tournaments</h3>
-              <p className="px-40 my-10">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident a, odit consequatur sint quis, accusamus ratione
-                sapiente repellendus odio et minima beatae ipsum cumque ipsa
-                labore quibusdam ullam adipisci aut.
+              <h3 className="my-10">How to select rackets and strings?</h3>
+              <p className="text-center px-40 my-10">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
+                tempora molestias dolore iusto autem error fuga neque explicabo,
+                amet voluptas quia assumenda adipisci! Atque veritatis ipsa
+                totam mollitia, fugiat dolore!
               </p>
-              <div className="flex justify-center">
-                <div className="mr-16 flex flex-col items-center">
-                  <div className="w-[20rem] h-[10rem]">
-                    {/* <img src={Atp} alt="" /> */}
+              <div>
+                <div className="flex my-20">
+                  <div className="w-1/2 flex flex-col items-center">
+                    <img className="w-[30rem]" src={Rackets} alt="" />
                   </div>
-                  <h4 className="my-10">
-                    ATP (Association of Tennis Professionals)
-                  </h4>
+                  <div className="w-1/2">
+                    <h3>Racket weight/face size</h3>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Natus libero sapiente doloribus itaque sunt quasi porro
+                      ipsam aliquid quidem tenetur minus odio, aspernatur,
+                      debitis repellat perferendis, at vel distinctio quis.
+                    </p>
+                  </div>
                 </div>
-                <div className="ml-16 flex flex-col items-center">
-                  <div className="w-[20rem] h-[10rem]">
-                    {/* <img src={Wta} alt="" /> */}
+                <div className="flex">
+                  <div className="w-1/2 flex flex-col items-center">
+                    <img className="w-[30rem]" src={String} alt="" />
                   </div>
-                  <h4 className="my-10">WTA (Women's Tennis Association)</h4>
+                  <div className="w-1/2">
+                    <h3>Strings types/tension</h3>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Natus libero sapiente doloribus itaque sunt quasi porro
+                      ipsam aliquid quidem tenetur minus odio, aspernatur,
+                      debitis repellat perferendis, at vel distinctio quis.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="flex flex-col items-center my-40">
-          <h2 className="text-[3rem] font-bold">Tournaments in Vancouver</h2>
+          <h2 className="text-[3rem] font-bold">Tennis shops in Vancouver</h2>
           <p className="text-center px-40 my-10">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
             ipsum quaerat at repellat dolore illo earum expedita suscipit odio
             dolores reiciendis, iusto similique nesciunt explicabo nobis ad
             esse, natus velit?
           </p>
-          <div className="my-20 grid lg:grid-rows-1 sm:grid-rows-2 grid-flow-col px-32">
+          <div className="my-20 grid lg:grid-rows-1 sm:grid-rows-3 grid-flow-col px-32">
             <div className="flex flex-col items-center text-center">
-              {/* <img className="w-50" src={Van} alt="" /> */}
-              <h2 className="my-10">Odlum Brown VanOpen</h2>
+              <img className="w-50" src={RR} alt="" />
+              <h2 className="my-10">Rackets & Runners</h2>
+              <p className="w-1/2">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                porro quo architecto nulla quisquam provident, nesciunt sit
+                placeat neque modi.
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <img className="w-50" src={K8} alt="" />
+              <h2 className="my-10">K8 Strings</h2>
+              <p className="w-1/2">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+                porro quo architecto nulla quisquam provident, nesciunt sit
+                placeat neque modi.
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <img className="w-50" src={Tad} alt="" />
+              <h2 className="my-10">Tad's Sporting Goods</h2>
               <p className="w-1/2">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
                 porro quo architecto nulla quisquam provident, nesciunt sit
