a3ef4e02ea2d4ad95b9dad59b3ea769021e1f776
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-04-22 22:45:00 -0700

    Updated padding and profile image size in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         <div className="flex flex-col items-center my-40 lg:pl-40 lg:pr-40">
           <h2 className="text-[3rem] font-bold">What's VTG?</h2>
           <img className="w-[15rem] lg:w-[30rem] my-20" src={Logo} alt="" />
-          <p className="text-center lg:pl-20 lg:pr-20 text-xl lg:text-2xl lg:leading-relaxed">
+          <p className="text-center lg:pl-20 lg:pr-20 text-lg lg:text-2xl lg:leading-relaxed">
             VTG(Vancouver Tennis Guide) is a website designed for anyone who is
             interested in playing tennis in Vancouver, whether they want to
             start playing or improve their tennis level. It is a place where you
@@ -33,23 +33,21 @@ const About = () => {
               />
             </div>
             <div className="lg:w-1/2 text-center lg:py-5 lg:px-10">
-              <p className="m-0 text-xl lg:text-2xl lg:leading-relaxed">
+              <p className="m-0 text-lg lg:text-2xl lg:leading-relaxed">
                 I started playing tennis when I was 15 years old. After
                 graduating from university, I have worked at a tennis brand
                 (DUNLOP/BabolaT). <br />
                 I’m currently studying programming in Vancouver and looking for
                 a job to become a web developer.
               </p>
-              <p className="mt-5 text-xl lg:text-2xl lg:leading-relaxed">
+              <p className="mt-5 text-lg lg:text-2xl lg:leading-relaxed">
                 This time I have shaped what I’m learning and what I can use
                 from my experience. I have compiled not only information on how
                 to improve your tennis, but also advice on gear, information on
                 tennis, and other information that you should keep in mind when
-                enjoying tennis in Vancouver. This will be helpful to enjoy
-                tennis more and give you the right advice for those who are
-                thinking about starting to play tennis.
+                enjoying tennis in Vancouver.
               </p>
-              <p className="mt-5 text-xl lg:text-2xl lg:leading-relaxed">
+              <p className="mt-5 text-lg lg:text-2xl lg:leading-relaxed">
                 This will be helpful to enjoy tennis more and give you the right
                 advice for those who are thinking about starting to play tennis.
               </p>
@@ -64,7 +62,7 @@ const About = () => {
             <div className="flex flex-col items-center text-center">
               <img className="w-[5rem] lg:w-[7rem]" src={Health} alt="" />
               <h2 className="my-10">Health</h2>
-              <p className="w-full lg:w-[40rem] text-xl lg:text-2xl lg:leading-relaxed">
+              <p className="text-xl lg:text-2xl lg:leading-relaxed lg:pl-20 lg:pr-20">
                 Creating opportunities to exercise through tennis and other
                 sports can keep your body and mental health well and improve
                 your physical. Tennis is a great opportunity to try other
@@ -74,7 +72,7 @@ const About = () => {
             <div className="flex flex-col items-center text-center mt-10 lg:mt-0">
               <img className="w-[5rem] lg:w-[7rem]" src={Group} alt="" />
               <h2 className="my-10">Social</h2>
-              <p className="w-full lg:w-[40rem] text-xl lg:text-2xl lg:leading-relaxed">
+              <p className="text-xl lg:text-2xl lg:leading-relaxed lg:pl-20 lg:pr-20">
                 Tennis cannot be played without a partner or opponent. You can
                 enjoy playing tennis with someone close to you, or you can try
                 to meet new people through tennis. Sports is like a language
